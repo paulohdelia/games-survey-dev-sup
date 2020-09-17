fd94fc376670b1c0202341b5e624791a2e7f189e
paulohdelia <paulohdelia@gmail.com>
2020-09-17 15:48:42 -0300

    create navigation with routes

--- front-web/src/components/Header/index.tsx
+++ front-web/src/components/Header/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 import './styles.css';
 
 import { ReactComponent as Logo } from '../../assets/logo.svg';
@@ -7,11 +9,13 @@ import { ReactComponent as Logo } from '../../assets/logo.svg';
 const Header: React.FC = () => {
   return (
     <header className="main-header">
-      <Logo />
-      <div className="logo-text">
-        <span className="logo-text-1">Big Game </span>
-        <span className="logo-text-2">Survey</span>
-      </div>
+      <Link className="main-header-link" to="/">
+        <Logo />
+        <div className="logo-text">
+          <span className="logo-text-1">Big Game </span>
+          <span className="logo-text-2">Survey</span>
+        </div>
+      </Link>
     </header>
   );
 };
