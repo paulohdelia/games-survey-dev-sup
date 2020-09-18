98c51bf956006013f330808bdc872ee15af2b307
paulohdelia <paulohdelia@gmail.com>
2020-09-18 16:25:33 -0300

    add routes - mobile

---
+++ front-mobile/src/pages/CreateRecord/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Header from '../../components/Header';
+
+const CreateRecord: React.FC = () => {
+  return <Header />;
+};
+
+export default CreateRecord;
--- front-mobile/src/pages/Home/index.tsx
+++ front-mobile/src/pages/Home/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Alert, Image, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 import { FontAwesome5 as Icon } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import Header from '../../components/Header';
 
 const Home: React.FC = () => {
+  const navigation = useNavigation();
+
   const handleOnPress = () => {
-    Alert.alert('ok');
+    navigation.navigate('CreateRecord');
   };
 
   return (
     <>
+      <Header />
       <View style={styles.container}>
         <Image
           style={styles.gamerImage}
